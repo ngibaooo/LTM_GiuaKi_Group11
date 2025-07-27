23ed0401c220220c5dc1b39955859c45e9238a7d
dtien18 <tiendt4180@ut.edu.vn>
2025-07-27 20:19:02 +0700

    bo sung ham xuly

--- locgic_game.py
+++ locgic_game.py
@@ -10,3 +10,9 @@ def determine_winner(player1_choice, player2_choice):
     """
     if player1_choice == player2_choice:
         return "Draw"
+    elif (player1_choice == "rock" and player2_choice == "scissors") or \
+         (player1_choice == "paper" and player2_choice == "rock") or \
+         (player1_choice == "scissors" and player2_choice == "paper"):
+        return "Player 1 wins"
+    else:
+        return "Player 2 wins"
